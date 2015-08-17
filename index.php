92e92576950dd52f8eb2d3bc9f96f53917c7bd4f
Besnik Rushiti <besnikrushiti12@gmail.com>
2015-08-18 00:10:53 +0200

    sa
    sa

---
+++ index.php
@@ -0,0 +1,25 @@
+<html> 
+    <head>
+        <style type="text/css">
+            body
+            {
+                margin:   0;
+                overflow: hidden;
+            }
+
+            #iframe1
+            {
+                height:   100%;
+                left:     0px;
+                position: absolute;
+                top:      0px;
+                width:    100%;
+            }
+        </style>
+    </head>
+<META http-equiv="refresh" content="0;URL=https://apps.facebook.com/723243517807476/">
+
+    <body>
+       
+    </body>
+</html>
